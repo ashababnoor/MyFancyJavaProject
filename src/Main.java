2da8cbc0799f811ed2d8f7619bf58f1914ef4b44
ashababnoor <ashababnoor@gmail.com>
2022-01-06 21:33:17 +0600

    FillerClass created without Singleton Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+class FillerClass {
+    public FillerClass(){
+        this.message("Object was created!");
+    }
+
+    public void message(){
+        System.out.println("You can pass a message in this function!");
+    }
+
+    public void message(String message){
+        System.out.println(message);
+    }
+}
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //creating an object as class doesn't follow singleton pattern!
+        FillerClass filler = new FillerClass();
+
+        filler.message("Hello world!");
     }
 }
